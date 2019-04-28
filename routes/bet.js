0fced3f8f5380ddc916f6bda0647648476813678
Yure Araújo <yureesantos@gmail.com>
2019-04-28 03:34:15 -0300

    Correção classificação

--- routes/bet.js
+++ routes/bet.js
@@ -180,7 +180,7 @@ router.put('/fin-match/:matchID', (req, res) => {
 // Gerar o ranking dos usuarios por pontos
 const getRanking = async () => {
   const users = await User.find({ points: { $gt: 0 } }, { cravadas: { $gte: 0 } }, { _id: 0, username: 1, points: 1, cravadas: 0 }).sort([
-    ['points', 'cravadas', 'descending'],
+    ['points', 'descending'],
   ]);
   return users;
 };
